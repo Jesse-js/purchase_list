7f039bf0990177892c93fb9309f5fe551898108b
Jesse-js <jesse-jjs@hotmail.com>
2024-04-18 23:57:24 -0300

    feat: add products crud

---
+++ app/Http/Requests/ProductRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ProductRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+			'name' => 'required|string',
+			'decription' => 'string',
+			'quantity' => 'required',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+    }
+}
---
+++ resources/views/product/form.blade.php
@@ -0,0 +1,24 @@
+<div class="row padding-1 p-1">
+    <div class="col-md-12">
+        
+        <div class="form-group mb-2 mb20">
+            <label for="name" class="form-label">{{ __('Name') }}</label>
+            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name', $product?->name) }}" id="name" placeholder="Name">
+            {!! $errors->first('name', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
+        </div>
+        <div class="form-group mb-2 mb20">
+            <label for="decription" class="form-label">{{ __('Decription') }}</label>
+            <input type="text" name="decription" class="form-control @error('decription') is-invalid @enderror" value="{{ old('decription', $product?->decription) }}" id="decription" placeholder="Decription">
+            {!! $errors->first('decription', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
+        </div>
+        <div class="form-group mb-2 mb20">
+            <label for="quantity" class="form-label">{{ __('Quantity') }}</label>
+            <input type="text" name="quantity" class="form-control @error('quantity') is-invalid @enderror" value="{{ old('quantity', $product?->quantity) }}" id="quantity" placeholder="Quantity">
+            {!! $errors->first('quantity', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
+        </div>
+
+    </div>
+    <div class="col-md-12 mt20 mt-2">
+        <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
+    </div>
+</div>
